6d291fb97b393e066d04843af8c49d19b1244b9e
Andrew Keck <andrewkeck@Andrews-MacBook-Pro.local>
2024-03-12 11:06:33 -0400

    fixed for vs code

--- scicalc.cpp
+++ scicalc.cpp
@@ -6,40 +6,39 @@
 
 using namespace std;
 
-//any mathematical equation entered using infix notaion
-//addition, subtraction, multiplication, and division
-//trigonometric and logarithmic functions
-//sin sec tan cos csc ctan 
-//ln log log2 log_b(x)
-//class defines data of calculator and various types of functions
+// any mathematical equation entered using infix notaion
+// addition, subtraction, multiplication, and division
+// trigonometric and logarithmic functions
+// sin sec tan cos csc ctan
+// ln log log2 log_b(x)
+// class defines data of calculator and various types of functions
 //+ - * / %
-//classes that handle different exceptions when illegal input or operation in imaginary/singular values
+// classes that handle different exceptions when illegal input or operation in imaginary/singular values
 
 void Scicalc::ScicalcRun()
 {
     char a;
     cout << "\nWelcome to the Scientific Calculator!\n";
     cout << "Here you are able to perform simple mathematical computations\nAs well as some more challenging trigonometric and logarithmic functions\n";
-    
+
     do
     {
         cout << "Enter h for some help with the complex equations of s to start now, or q to quit: ";
         cin >> a;
         switch (a)
         {
-            case 'h':
-                help();
-                break;
-            case 's':
-                openCalc();
-                break;
-            default:
-                sciEnd = true;
-                break;
+        case 'h':
+            help();
+            break;
+        case 's':
+            openCalc();
+            break;
+        default:
+            sciEnd = true;
+            break;
         }
 
-        
-    }while(!sciEnd);
+    } while (!sciEnd);
 }
 
 void Scicalc::help()
@@ -54,73 +53,85 @@ void Scicalc::help()
 
 void Scicalc::openCalc()
 {
-    //user types in any equations
-    //parse out what kind of equation it is
-    //contains ln... or sin...?
-    //if not, take infix, convert to postfix, perform
-    //if does, determine if logarithmic or Trigonometric
-    //perform those calculations
-    
-    //need 3 classes
-    //arithmatic, log, trig
-    //arithmatic, in to post
-    
+    // user types in any equations
+    // parse out what kind of equation it is
+    // contains ln... or sin...?
+    // if not, take infix, convert to postfix, perform
+    // if does, determine if logarithmic or Trigonometric
+    // perform those calculations
+
+    // need 3 classes
+    // arithmatic, log, trig
+    // arithmatic, in to post
+
     cout << "\nEquation: ";
     cin >> ws;
     getline(cin, input);
-    
-    parseInput(input);
 
+    parseInput(input);
 }
 
 void Scicalc::parseInput(string input)
 {
-    vector<string> logs = {"ln", "log"};
-    vector<string> trigs = {"sin", "sec", "tan", "cos", "csc", "ctan"};
-    
-    if(contains_any(input, logs))
+    vector<string> logs;
+    logs.push_back("ln");
+    logs.push_back("log");
+    vector<string> trigs;
+    trigs.push_back("sin");
+    trigs.push_back("sec");
+    trigs.push_back("tan");
+    trigs.push_back("cos");
+    trigs.push_back("csc");
+    trigs.push_back("ctan");
+
+    if (contains_any(input, logs))
     {
-        //send to logs
+        // send to logs
         Logs logars;
         cout << "Send to logs\n";
     }
-    else if(contains_any(input, trigs))
+    else if (contains_any(input, trigs))
     {
-        //send to trigs
+        // send to trigs
         Trigs trig;
         cout << "Send to trigs\n";
     }
     else
     {
         regex pattern("[0-9()+\\-*/%\\s]+");
-        if(!regex_match(input, pattern)){
+        if (!regex_match(input, pattern))
+        {
             cout << "Incorrect input, try again\n";
             return;
         }
-        //send to ariths
+        // send to ariths
         cout << "Send to ariths\n";
         Ariths arith;
         arith.infixToPostfix(input);
     }
 }
 
-bool Scicalc::contains_any(const string& str, const vector<string>& phrases) {
-  for (const string& phrase : phrases) {
-    if (str.find(phrase) != string::npos) {
-      return true;
+bool Scicalc::contains_any(const string &str, const vector<string> &phrases)
+{
+    for (const string &phrase : phrases)
+    {
+        if (str.find(phrase) != string::npos)
+        {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
 
 void Ariths::infixToPostfix(string infixEquation)
 {
-    if(infixEquation != "")
+    if (infixEquation != "")
     {
-        //need operator precedence
-        //read through infix to create postfix (stack)
+        // need operator precedence
+        // read through infix to create postfix (stack)
     }
-    else {
+    else
+    {
         cout << "Incorrect input, try again\n";
     }
 }
@@ -137,7 +148,8 @@ int Ariths::precedence(char c)
         return -1;
 }
 
-void Ariths::calculateArith(string postfixEquation){
-    
-    //run through postfix to calculate (stack)
+void Ariths::calculateArith(string postfixEquation)
+{
+
+    // run through postfix to calculate (stack)
 }
